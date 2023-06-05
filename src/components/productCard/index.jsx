d001bceb988a35dfc3ec6e87b337044cb0aee1ec
luan <luan.barreiros@rethink.dev>
2023-06-05 18:22:59 -0300

    card component

---
+++ src/components/productCard/index.jsx
@@ -0,0 +1,24 @@
+import "./styles.css";
+import CustomizedRating from "../starRating";
+
+const Card = ({ img, name, weigth, rating, rate, price }) => {
+  return (
+    <div className="cardContainer">
+      <div className="cardImageContainer">
+        <img alt="product" src={img}></img>
+      </div>
+      <div className="cardContentContainer">
+        <p className="cardDescription">
+          {name} | {weigth}
+        </p>
+        <p className="cardRating">
+          <CustomizedRating rating={rate} precision={0.1}></CustomizedRating>{" "}
+          {rating} avaliações
+        </p>
+        <p className="cardPrice">R$ {price.toFixed(2)}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
