3d85a77764f130c9a5071289a1aa87a46b7b2ccb
luan <luan.barreiros@rethink.dev>
2023-06-05 17:04:09 -0300

    changed the style flow

--- src/components/starRating/index.jsx
+++ src/components/starRating/index.jsx
@@ -1,43 +1,24 @@
-import StarBorderIcon from "@mui/icons-material/StarBorder";
-import { styled } from "@mui/material/styles";
 import Rating from "@mui/material/Rating";
-import { makeStyles } from "@mui/styles";
-import Box from "@mui/material/Box";
-
-const StyledRating = styled(Rating)({
-  emptyStar: {
-    color: "#694948",
-  },
-  "& .MuiRating-iconFilled": {
-    color: "#694948",
-  },
-});
-
-const useStyles = makeStyles(() => ({
-  emptyStar: {
-    color: "#694948",
-  },
-}));
 
 const CustomizedRating = ({ rating, precision }) => {
-  const styled = useStyles();
+  const styledStar = {
+    color: "success.main",
+    "& .css-1c99szj-MuiRating-icon": {
+      color: "#694948",
+    },
+    "& .MuiRating-iconFilled": {
+      color: "#694948",
+    },
+  };
   return (
-    <Box
-      sx={{
-        "& > legend": { mt: 2 },
-      }}
-    >
-      <StyledRating
-        name="customized-color"
-        value={rating}
-        getLabelText={(value) => `${value} Heart${value !== 1 ? "s" : ""}`}
-        precision={precision || 0.5}
-        readOnly
-        emptyIcon={
-          <StarBorderIcon fontSize="inherit" className={styled.emptyStar} />
-        }
-      />
-    </Box>
+    <Rating
+      name="customized-color"
+      value={rating}
+      getLabelText={(value) => `${value} Heart${value !== 1 ? "s" : ""}`}
+      precision={precision || 0.5}
+      readOnly
+      sx={styledStar}
+    />
   );
 };
 
