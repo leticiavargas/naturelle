fcabe628977a36911feed442408a4012087dade2
luan <luan.barreiros@rethink.dev>
2023-06-07 15:49:25 -0300

    first component

---
+++ src/components/carousel/index.jsx
@@ -0,0 +1,81 @@
+import { useSwipeable } from "react-swipeable";
+import imagem from "../../assets/dif.svg";
+import React, { useState } from "react";
+import "./styles.css";
+
+const Carousel = ({ images }) => {
+  images = [imagem, imagem, imagem];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [offsetX, setOffsetX] = useState(0);
+  const [startX, setStartX] = useState(0);
+
+  const handleSwiping = (event) => {
+    const { deltaX, absX } = event;
+    const threshold = 50;
+
+    if (absX > threshold) {
+      setStartX(deltaX);
+      setOffsetX(deltaX);
+    } else {
+      setOffsetX(deltaX - startX);
+    }
+  };
+  const handlers = useSwipeable({
+    onSwipedLeft: () => showNextImage(),
+    onSwipedRight: () => showPrevImage(),
+    onSwiping: handleSwiping,
+  });
+
+  const showNextImage = () => {
+    const nextIndex = (currentIndex + 1) % images.length;
+    if (nextIndex === 0) {
+      setCurrentIndex(images.length - 1);
+    } else {
+      setCurrentIndex(nextIndex);
+    }
+    setOffsetX(0);
+  };
+
+  const showPrevImage = () => {
+    const prevIndex = (currentIndex - 1 + images.length) % images.length;
+    if (prevIndex === images.length - 1) {
+      setCurrentIndex(0);
+    } else {
+      setCurrentIndex(prevIndex);
+    }
+    setOffsetX(0);
+  };
+
+  return (
+    <div className="carousel-container">
+      <div
+        className="carousel"
+        {...handlers}
+        style={{
+          transform: `translateX(calc(-${currentIndex}00% + ${offsetX}px))`,
+        }}
+      >
+        {images.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt={`${index}`}
+            className={`carousel-image ${
+              index === currentIndex ? "active" : ""
+            }`}
+          />
+        ))}
+      </div>
+      <div className="indicators">
+        {images.map((_, index) => (
+          <div
+            key={index}
+            className={`indicator ${index === currentIndex ? "active" : ""}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
