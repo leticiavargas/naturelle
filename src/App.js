e2c37684cf1b7c881a53edd1b768fb62297769ed
Matheus Dias <matheus.dias@rethink.dev>
2023-06-07 15:13:55 -0300

    Removendo header do App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import "./App.css";
-import Header from "./components";
 
 function App() {
   return (
     <div className="App">
-      <Header title="Algo aqui" linkback="http://www.google.com" />
     </div>
   );
 }
