62294e3e030d41b012125e89ac5ef1de50a1c430
thiagoFirsen <thiago.firsen@rethink.dev>
2023-06-07 14:57:14 -0300

    TF - Finalizing Input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import "./App.css";
-import Header from "./components/header";
-import Login from "./pages/Login";
 
 function App() {
-  return (
-    <div className="App">
-      <Header title="Algo aqui" linkback="http://www.google.com" />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
