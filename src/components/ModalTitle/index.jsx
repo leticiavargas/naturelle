d0510aa75f0dd9dd847656fb969dac1333610af1
ribYuriRethink <yuri.maycon@rethink.dev>
2023-06-09 19:12:46 -0300

    refactoring after changes request

--- src/components/ModalTitle/index.jsx
+++ src/components/ModalTitle/index.jsx
@@ -3,14 +3,12 @@ import expandMoreIcon from "./expand_more.svg";
 
 const ModalTitle = ({ text, className, onClick }) => {
   return (
-    text && (
-      <div className={`modalTitle_component ${className}`}>
-        <h1>{text}</h1>
-        <button className="expand_more_button" onClick={onClick}>
-          <img src={expandMoreIcon} alt="expand more icon" />
-        </button>
-      </div>
-    )
+    <div className={`modalTitle_component ${className}`}>
+      {text && <h1>{text}</h1>}
+      <button className="expand_more_button" onClick={onClick}>
+        <img src={expandMoreIcon} alt="expand more button" />
+      </button>
+    </div>
   );
 };
 
