27ca70dff86109cefdc84cbd918de42badae6a20
ribYuriRethink <yuri.maycon@rethink.dev>
2023-06-09 15:39:04 -0300

    some changes on HyperLink

--- src/components/HyperLink/index.jsx
+++ src/components/HyperLink/index.jsx
@@ -1,13 +1,13 @@
 import "./styles.css";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const HyperLink = ({ text, className, toLink = "#" }) => {
+const HyperLink = ({ text, className, toLink = "#", onClick }) => {
   return (
     <div className={`hyperlink_container ${className}`}>
       {text && (
-        <NavLink to={toLink}>
+        <Link to={toLink} onClick={onClick}>
           <p>{text}</p>
-        </NavLink>
+        </Link>
       )}
     </div>
   );
