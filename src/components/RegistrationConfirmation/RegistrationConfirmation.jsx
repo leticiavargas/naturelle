ec08dde91b4a8774bdee32680a2ddb9a86928e31
Leticia-Guimaraes <124922074+Leticia-Guimaraes@users.noreply.github.com>
2023-06-09 12:52:34 -0300

    First Commit

---
+++ src/components/RegistrationConfirmation/RegistrationConfirmation.jsx
@@ -0,0 +1,29 @@
+import "./styles.css";
+import checkIcon from "../../assets/images/check-circle.svg";
+const RegistrationConfirmation = () => {
+  const [user, setUser] = useState("");
+
+  useEffect(() => {
+    setUser("Júlia");
+
+    // Aqui irá o retorno da rota criada
+    // getUserById()
+    //   .then((result) => {
+    //     setUser(result);
+    //   })
+    //   .catch((error) => {
+    //     alert(error);
+    //   });
+  }, []);
+  return (
+    <body className="container">
+      <img src={checkIcon} alt="check-icon" />
+      <text className="registrationConfirmation">
+        <h1>Boas-vindas, {user}!</h1>
+        <p>Seus dados foram salvos com sucesso!</p>
+      </text>
+    </body>
+  );
+};
+
+export default RegistrationConfirmation;
