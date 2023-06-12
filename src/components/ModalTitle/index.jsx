8284db3f450fb30ab03303d3ed21c46349358ed0
ribYuriRethink <yuri.maycon@rethink.dev>
2023-06-12 12:47:37 -0300

    changes on variables names

--- src/components/ModalTitle/index.jsx
+++ src/components/ModalTitle/index.jsx
@@ -1,12 +1,12 @@
 import "./styles.css";
-import expandMoreIcon from "./expand_more.svg";
+import expandIcon from "./expand_more.svg";
 
 const ModalTitle = ({ text, className, onClick }) => {
   return (
     <div className={`modalTitle_component ${className}`}>
       {text && <h1>{text}</h1>}
-      <button className="expand_more_button" onClick={onClick}>
-        <img src={expandMoreIcon} alt="expand more button" />
+      <button className="expand_button" onClick={onClick}>
+        <img src={expandIcon} alt="expand button" />
       </button>
     </div>
   );
