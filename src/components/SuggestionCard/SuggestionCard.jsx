0a6d622feb0d581f68b8fb28f6ffa20152d071ea
Marcos Paulo <marcos.pereira@rethink.dev>
2023-06-12 17:57:40 -0300

    Code correction #1
    Changes made to the component .JSX and CSS considering previous code reviews on the onClick and the logic itself.

--- src/components/SuggestionCard/SuggestionCard.jsx
+++ src/components/SuggestionCard/SuggestionCard.jsx
@@ -2,48 +2,47 @@ import "./SuggestionCard.css";
 import ProductImageCard from "../ProductCardImage/ProductCardImage";
 import { FiShoppingCart } from "react-icons/fi";
 
-const SuggestionCard = ({
-  className,
-  productName,
-  productImage,
-  productContent,
-  productPrice,
-  installments,
-}) => {
+const SuggestionCard = ({ className, product, installments, onClick }) => {
+  const capitalize = (string) =>
+    string.split("").reduce((acc, element, index, array) => {
+      acc +=
+        !index ||
+        (string.charAt(index - 1) === " " && string.charAt(index + 2) !== " ")
+          ? element.toUpperCase()
+          : element;
+
+      return acc;
+    }, "");
+
   const BRReal = Intl.NumberFormat("pt-BR", {
     style: "currency",
     currency: "BRL",
   });
 
+  const name = capitalize(product.name);
+  const { tags, price } = product;
+  const [image] = product.images;
+
   return (
-    <>
-      <div className={`SuggestionCard ${className}`}>
-        <ProductImageCard
-          className="ImageCard"
-          productAltText={productName}
-          productImage={productImage}
-          productPrice={productPrice}
-          hasSugar={productContent.sugar}
-          hasGluten={productContent.gluten}
-          hasMilk={productContent.milk}
-        />
-        <div className="DescriptionContainer">
-          <h1>{productName}</h1>
-          <h2>{BRReal.format(productPrice)}</h2>
-          <p>{`Em até ${installments}x sem juros`}</p>
-        </div>
-          <button
-            className="ShoppingCartButton"
-            onClink={() => {
-              //Inserir função que adiciona produto ao carrinho
-              // ou abre modal para adicionar um produto com quantidade
-              console.log("Botão pressionado");
-            }}
-          >
-            <FiShoppingCart className="Icon" />
-          </button>
+    <div className={`SuggestionCard ${className}`}>
+      <ProductImageCard
+        className="ImageCard"
+        productAltText={name}
+        productImage={image}
+        productPrice={price}
+        hasSugar={!tags.includes("sugarFree")}
+        hasGluten={!tags.includes("glutenFree")}
+        hasMilk={tags.includes("milkFree")}
+      />
+      <div className="DescriptionContainer">
+        <h1>{name}</h1>
+        <h2>{BRReal.format(price)}</h2>
+        <p>{`Em até ${installments}x sem juros`}</p>
       </div>
-    </>
+      <button className="ShoppingCartButton" onClick={onClick}>
+        <FiShoppingCart className="Icon" />
+      </button>
+    </div>
   );
 };
 
