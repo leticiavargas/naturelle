c8180d046287a9d3d2a047e54dad6edf68d685d6
André Fonseca <andre.fonseca@rethink.dev>
2023-06-12 14:17:34 -0300

    Padronizando os nomes dos arquivos

--- src/components/CarouselInstitucional/index.jsx
+++ src/components/CarouselInstitucional/index.jsx
@@ -1,27 +1,25 @@
-import './CarouselInstitucional.css'
+import "./styles.css";
 
-const CarouselInstitucional = ({ title='', data=[] }) => {
-
+const CarouselInstitucional = ({ title = "", data = [] }) => {
   return (
-    <div className='container'>
+    <div className="container">
       {title && <h1>{title}</h1>}
-       
-        <div className="carousel">
-          {data && data.map((image, index) => (
-            <div className='image-container' key={index}>
+
+      <div className="carousel">
+        {data &&
+          data.map((image, index) => (
+            <div className="image-container" key={index}>
               <img src={image.url} alt={image.alt} />
-              {image.title &&
-              <a href={"http://localhost:3000/" + image.goTo}>
-                <button className='button-cover'>
-                  {image.title}
-                </button>
-              </a>
-              }
+              {image.title && (
+                <a href={"http://localhost:3000/" + image.goTo}>
+                  <button className="button-cover">{image.title}</button>
+                </a>
+              )}
             </div>
           ))}
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default CarouselInstitucional;
