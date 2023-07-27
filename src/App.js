7d6d93e8411be39932d35ee446425c28ec4ea8ec
leticia.vargas <le.mvargas@gmail.com>
2023-07-27 15:01:54 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Login/>} />
         <Route path="*" element={"404"} />
       </Routes>
     </BrowserRouter>
